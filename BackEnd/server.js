45b7a803a94474af8ff74ddc943e375f895dafed
NabeelWaq1 <nabeelwaqas24@gmail.com>
2024-09-27 16:51:22 +0500

    fixed folder name

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -18,10 +18,10 @@ app.use('/api/products',productRouter)
 const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.use(express.static(path.join(__dirname,"/FrontEnd/dist")));
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'));
+        res.sendFile(path.resolve(__dirname,'FrontEnd','dist','index.html'));
     })
 }
 
